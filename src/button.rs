e829532e8ddf6f82e6e1facd154f5b73694f0f5e
mimi <allthatjazz@outlook.com>
2021-04-21 22:53:02 -0400

    button plugin

---
+++ src/button.rs
@@ -0,0 +1,48 @@
+use bevy::prelude::*;
+
+pub struct StylePlugin;
+
+impl Plugin for StylePlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        app.add_system(set_display.system());
+    }
+}
+
+//select classes
+pub struct Display;
+pub struct Normal;
+
+//interaction
+pub enum Status {
+    Focused,
+    Normal,
+    Disabled,
+}
+
+pub type ColorHandle = Handle<ColorMaterial>;
+
+//style resources
+pub struct DisplayStyle {
+    normal: ColorHandle,
+    focused: ColorHandle,
+    disabled: ColorHandle,
+}
+/* pub struct NormalStyle {
+    normal: ColorHandle,
+    focused: ColorHandle,
+} */
+
+fn set_display(
+    mut query: Query<(&mut ColorHandle, &Status), (With<Display>,)>,
+    style: Res<DisplayStyle>,
+) {
+    query.iter_mut().for_each(|(mut material, st)| {
+        *material = {
+            match st {
+                Status::Focused => style.focused.clone(),
+                Status::Disabled => style.disabled.clone(),
+                _ => style.normal.clone(),
+            }
+        }
+    });
+}
